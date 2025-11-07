747c829f433529503626c040e486be85fccc619a
Kuldeep Singh <kuldeepsinghrawat123321@gmail.com>
2025-11-07 23:29:07 +0530

    Added more Functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const plus= document.querySelector('.inc')
 const count=document.querySelector('.count')
 const input=document.querySelector('.id')
 const zero=document.querySelector('.zero')
+const mult=document.querySelector('.mul')
+const div=document.querySelector('.div')
 plus.addEventListener('click',()=>{
     const countValue=parseInt(count.innerText);
     const inputvalue=parseInt(input.value) || 1;
@@ -12,6 +14,16 @@ minus.addEventListener('click',()=>{
     const countValue=parseInt(count.innerText);
     const inputvalue=parseInt(input.value) || 1;
     count.innerText=countValue-inputvalue
+})
+mult.addEventListener('click',()=>{
+    const countValue=parseInt(count.innerText)
+    const inputValue=parseInt(input.value) || 2
+    count.innerText=countValue*(inputValue) 
+})
+div.addEventListener('click',()=>{
+    const countValue=parseInt(count.innerText)
+    const inputValue=parseInt(input.value) || 2
+    count.innerText=countValue/(inputValue)
 })
 zero.addEventListener('click',()=>{
     count.innerText=0;
